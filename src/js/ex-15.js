da1c9a9dcd066f409c3626f6f225717d2536cb16
Andrespz07 <andresperezortega2001@gmail.com>
2023-01-23 21:27:10 +0100

    ejercicio 15

---
+++ src/js/ex-15.js
@@ -0,0 +1,21 @@
+const button = document.getElementById("buttonPrimo");
+
+button.onclick = function esPrimo() {
+    let numero = prompt("Introduce un número")
+
+    for(let i = 2,raiz=Math.sqrt(numero); i <= raiz; i++)
+    if(numero % i === 0){
+        numero = false;
+       document.getElementById("numeroPrimo").innerHTML = numero; 
+    }
+    if(numero > 1){
+        numero = true;
+        document.getElementById("numeroPrimo").innerHTML = numero
+    }
+
+}  
+
+
+
+// if(numero % i === 0) return false;
+// return numero > 1;
